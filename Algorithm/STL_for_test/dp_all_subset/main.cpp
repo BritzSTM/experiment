e4d3bce0c7928fa40c2df5daf0eec17db4c0ee06
BritzSTM <30376375+BritzSTM@users.noreply.github.com>
2021-02-12 02:46:59 +0900

    Tuto memoization

--- Algorithm/STL_for_test/dp_all_subset/main.cpp
+++ Algorithm/STL_for_test/dp_all_subset/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include <numeric>
+#include <chrono>
 
 
 using namespace std;
@@ -30,7 +31,7 @@ bool GetAllSubsets_bf(const vector<int>& set, int target)
 	bool found{ false };
 	for (auto size{ 0 }; size < allSubsets.size(); ++size)
 	{
-		cout << "부분집합의 원소 갯수 : " << size << endl;
+		//cout << "부분집합의 원소 갯수 : " << size << endl;
 
 		for (const auto& subset : allSubsets[size])
 		{	
@@ -38,10 +39,10 @@ bool GetAllSubsets_bf(const vector<int>& set, int target)
 			
 			for_each(cbegin(subset), cend(subset), [&sum](const auto& v) {
 				sum += v;
-				cout << v << " ";
+				//cout << v << " ";
 				});
 
-			cout << endl;
+			//cout << endl;
 			if (sum == target)
 				found = true;
 		}
@@ -50,11 +51,50 @@ bool GetAllSubsets_bf(const vector<int>& set, int target)
 	return found;
 }
 
+bool GetAllSubsets_memo(const vector<int>& set, int sum, int i, vector<vector<int>>& memo)
+{
+	if (sum == 0)
+		return true;
+
+	if (i == set.size() || set[i] > sum)
+		return false;
+
+	// 현재 요소의 합계상태
+	if (memo[i][sum] == -1)
+	{
+		bool append = GetAllSubsets_memo(set, sum - set[i], i + 1, memo);
+		bool ignore = GetAllSubsets_memo(set, sum, i + 1, memo);
+
+		memo[i][sum] = append | ignore;
+	}
+
+	return memo[i][sum];
+}
+
 int main(void)
 {
-	vector<int> set{ 13, 79, 45, 29 };
+	vector<int> set{ 
+		16, 1058, 22, 13, 46, 55, 3, 92, 47, 
+		7, 98, 367, 807, 106, 333, 85, 577, 9, 3059 };
 	
-	cout << "FT"[static_cast<int>(GetAllSubsets_bf(set, 58))];
+	const int target{ 6799 };
+
+	using namespace chrono;
+
+	cout << "BF\n";
+	auto st{ steady_clock::now() };
+	cout << "FT"[static_cast<int>(GetAllSubsets_bf(set, target))] << endl;
+	auto et{ steady_clock::now() };
+	cout << duration_cast<microseconds>(et - st).count() << endl;
+	
+	cout << "Memo\n";
+	st = steady_clock::now();
+	{
+		vector<vector<int>> memo(set.size(), vector<int>(7000, -1));
+		cout << "FT"[static_cast<int>(GetAllSubsets_memo(set, target, 0, memo))] << endl;
+	}
+	et = steady_clock::now();
+	cout << duration_cast<microseconds>(et - st).count() << endl;
 
 	return 0;
 }
